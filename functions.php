2a78d94251e5f8d8d451f2a892ad62cd0207c838
arisa_naiyun <arisa920118@gamil.com>
2023-04-23 21:40:15 +0800

    revise submit4

--- functions.php
+++ functions.php
@@ -20,8 +20,8 @@ $image=$targetFilePath;
 // if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
     // if(!empty($_FILES["file"]["name"])){
     // Allow certain file formats
-    $allowTypes = array('jpg','png','jpeg','gif','pdf');
-    if(in_array($fileType, $allowTypes)){
+    // $allowTypes = array('jpg','png','jpeg','gif','pdf');
+    // if(in_array($fileType, $allowTypes)){
         // Upload file to server
         if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
             // Insert image file name into database
@@ -52,9 +52,9 @@ $image=$targetFilePath;
         }else{
             $statusMsg = "Sorry, there was an error uploading your file.";
         }
-    }else{
-        $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
-    }
+    // }else{
+    //     $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+    // }
 // }else{
     // $statusMsg = 'Please select a file to upload.';
 
