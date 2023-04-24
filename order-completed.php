6752e7867209a86c962d576ab33a6228874ae958
arisa_naiyun <arisa920118@gamil.com>
2023-04-25 07:55:16 +0800

    revise beauty3

--- order-completed.php
+++ order-completed.php
@@ -1,67 +1,81 @@
-<?php include('header_color.php'); ?>
-<div class="colorBG" >
+<?php include_once('dbConnect.php'); ?>
+<!DOCTYPE html>
+<html>
 
-</div>
-<div class="TITLE">
-    
-<h1 id="text1">Flower</h1>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="UseCSS3.css" rel="stylesheet">
+    <title>
+        flower
+    </title>
 
-</div>
+</head>
 
-<div class="TITLE_flower1" >
-<img src="img\IMG_2223.PNG" id="flowers" width="100%">
-</div>
-<div class="TITLE_flower">
-<img src="img\flowers2.PNG" id="flowers" width="100%">
-</div>
-<div class="cover">
+<body>
+    <nav>
+        <ul class="clientMenu">
+            <li class="title">flower</li>
+            <li><a href="/">home</a></li>
+            <li><a href="/#flower_intro_call">About</a></li>
+        </ul>
+    </nav>
 
-</div>
-<div class="TITLE_note">
-<img src="img\flower_frame.PNG" id="flowers" width="100%">
-</div>
-<div class="TITLE_note1">
-<div class="note1_container">
-<?php
-include_once('dbConnect.php');
-$sql = "SELECT * FROM person_data ORDER BY RAND ( ) LIMIT 1  ";
-$result = mysqli_query($dbConnection,$sql);
-$row = mysqli_fetch_assoc($result);
-if($row['person_name']=="匿名"){
-    echo $row['person_name'].'('.$row['gender'].'):<br>';
-}else{
-    echo $row['person_name'].':<br>';
-}
-echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$row['text'];
-  
- 
-?>
-</div>
-</div>
 
-<div class="container">
-	<p>請點擊下方下載app 見證花朵的生命力</p>
-</div>
-<div class="text_Download">
-
-<p>Android</p>
-<p>ios</p>
-</div>
-<div class="icon_Download">
-
-<img src="img\andriod2.PNG" id="flowers" width="50%" >
-<img src="img\ios2.PNG" id="flowers" width="50%" >
-</div>
-<div class="btn_Download">
-
-<button>下載</button>
-<button>下載</button>
-</div>
+    <div class="content">
 
 
 
+        <div class="logo2">
+            <img src="img\logo2.PNG" width="100%">
+        </div>
+
+
+        <div class="flower_frame">
+            <img src="img\flower_frame.PNG" id="flowers" width="100%">
+
+            <div class="TITLE_note1">
+            <?php
+            include_once('dbConnect.php');
+                    $sql = "SELECT * FROM person_data ORDER BY RAND ( ) LIMIT 1  ";
+                    $result = mysqli_query($dbConnection, $sql);
+                    $row = mysqli_fetch_assoc($result);
+                    if ($row['person_name'] == "匿名") {
+                        echo $row['person_name'] . '(' . $row['gender'] . '):<br>';
+                    } else {
+                        echo $row['person_name'] . ':<br>';
+                    }?>
+                <div class="note1_container">
+                    <?php
+                    
+                    echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $row['text'];
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="icon_Download">
+            <img src="img\download.PNG"  width="100%">
+        </div>
+        <div class="btn_Download">
+<button onclick="location.href='uploadApk/flower.apk'" style="color: white; background:#78929ad9;border:2px soild white;">下載APK</button>
+            <button>未開放下載</button>
+        </div>
+
+
+    </div>
 
 
 
+    <footer>
 
-<?php include('footer_color.php'); ?>
+        <p>&copy;&thinsp;
+            <?php echo date('Y') ?>&thinsp;nyxc 版權所有 不得轉載
+        </p>
+    </footer>
+
+
+</body>
+
+</html>
