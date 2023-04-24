16a03c8c496eb238e1edac08ae1f37a3d180afe5
arisa_naiyun <arisa920118@gamil.com>
2023-04-24 10:35:00 +0800

    revise imgView4

--- imageView.php
+++ imageView.php
@@ -4,7 +4,7 @@ $sql = "SELECT * FROM person_data";
 $result = mysqli_query($dbConnection,$sql);
 while($row = mysqli_fetch_assoc($result)) {
 
-  echo '<img src=https://nyxc.azurewebsites.net/preview_img/'.$row['image_name'].'" width="200" /><br/>';
+  echo '<img src="https://nyxc.azurewebsites.net/preview_img/'.$row['image_name'].'" width="200" /><br/>';
 }
 
 ?>
