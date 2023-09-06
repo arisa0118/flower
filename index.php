0287ce3952cfc6dc8f94bba3c056378cef40174f
arisa_naiyun <arisa920118@gamil.com>
2023-09-06 17:06:48 +0800

    version1.7

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <!DOCTYPE html>
 <html>
-<?php include_once('dbConnect.php');?>
+<?php include('dbConnect.php');?>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
