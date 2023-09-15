baa719575211251274a4c6c603cccdb08ae46498
arisa_naiyun <arisa920118@gamil.com>
2023-09-16 00:21:27 +0800

    version4.2

--- dbConnect.php
+++ dbConnect.php
@@ -13,16 +13,12 @@ $database = "flower";
 // $database = "flower_picture"; 
 // echo "12行";
 $dbConnection=mysqli_connect($db_hostname,$db_username,$db_password,$database);
-
 //檢查連線是否成功
-if(mysqli_connect_errno()){
-    
+if(mysqli_connect_errno()){ 
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }else{
     //echo "成功連線";
 }
-
-
 //將文字編碼設為UTF-8以正確顯示中文
 mysqli_set_charset($dbConnection,"utf8");
 ?>
