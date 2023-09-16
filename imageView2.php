ac263b689f0318b725fe0024a5e82ea2cacb26f5
arisa_naiyun <arisa920118@gamil.com>
2023-09-16 18:01:43 +0800

    version4.7

--- imageView2.php
+++ imageView2.php
@@ -1,33 +1,33 @@
 <?php 
-// header("Access-Control-Allow-Origin: *");
-// //header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-// header("Access-Control-Allow-Methods: *");
-// header("Access-Control-Allow-Headers:  Origin, Methods,Content-Type");
+header("Access-Control-Allow-Origin: *");
+//header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers:  Origin, Methods,Content-Type");
 
 
-// $db_hostname = "nyxc1.mysql.database.azure.com";
-// $db_username = "nyxc_mySQL";
-// $db_password = "@Arisa0118Cassie0429";
-// $database = "flower"; 
-// // $db_hostname = "127.0.0.1";
-// // $db_username = "root";
-// // $db_password = "";
-// // $database = "flower_picture"; 
-// // echo "12行";
-// $dbConnection=mysqli_connect($db_hostname,$db_username,$db_password,$database);
+$db_hostname = "nyxc1.mysql.database.azure.com";
+$db_username = "nyxc_mySQL";
+$db_password = "@Arisa0118Cassie0429";
+$database = "flower"; 
+// $db_hostname = "127.0.0.1";
+// $db_username = "root";
+// $db_password = "";
+// $database = "flower_picture"; 
+// echo "12行";
+$dbConnection=mysqli_connect($db_hostname,$db_username,$db_password,$database);
 
-// //檢查連線是否成功
-// if(mysqli_connect_errno()){
+//檢查連線是否成功
+if(mysqli_connect_errno()){
     
-//     die('Failed to connect to MySQL: '.mysqli_connect_error());
-// }else{
-//     //echo "成功連線";
-// }
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}else{
+    //echo "成功連線";
+}
 
 
-// //將文字編碼設為UTF-8以正確顯示中文
-// mysqli_set_charset($dbConnection,"utf8");
-include_once('dbConnect.php');
+//將文字編碼設為UTF-8以正確顯示中文
+mysqli_set_charset($dbConnection,"utf8");
+//include_once('dbConnect.php');
 
 $sql = "SELECT * FROM person_data";
 $result = mysqli_query($dbConnection,$sql);
