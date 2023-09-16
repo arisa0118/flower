75c51890267b9255eadd0d37395685dca0298246
arisa_naiyun <arisa920118@gamil.com>
2023-09-16 18:03:09 +0800

    version4.8

--- dbConnect.php
+++ dbConnect.php
@@ -1,8 +1,8 @@
 <?php
 // if ( ini_set( 'upload_tmp_dir', '/home/site/wwwroot/preview_img' ) === false ) die( 'Unable to set upload_tmp_dir.' );
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Headers:  Origin, Methods,Content-Type"); 
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: *");
+// header("Access-Control-Allow-Headers:  Origin, Methods,Content-Type"); 
 
 
 $db_hostname = "nyxc1.mysql.database.azure.com";
