134149e89d2207ed20546e034807424be1ff3515
arisa_naiyun <arisa920118@gamil.com>
2023-10-15 23:45:01 +0800

    version7.1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <?php 
-include('dbConnect.php');?>
+// include('dbConnect.php');?>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,10 +21,13 @@ include('dbConnect.php');?>
 <iframe width="100%" height="100%" src="https://www.youtube.com/embed/HxHDHd4uPqM?autoplay=1" title="flower" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </div>	 -->
 <div class="main" id="main">
-<p></p>
+
     <img src="img\bg_3.PNG" width="100%" >
 </div>
-
+<div class="main_text" id="main_text">
+<p> </p>
+ 
+</div>
 
 <div class="btn_Download">
 <button onclick="location.href='/uploadPicture.php'">前往上傳最美好的回憶</button>   
