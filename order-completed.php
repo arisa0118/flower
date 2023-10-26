0e13443d5e19c3a339477973abc9aed41c30a07f
arisa_naiyun <arisa920118@gamil.com>
2023-10-26 19:32:08 +0800

    version8.2

--- order-completed.php
+++ order-completed.php
@@ -42,7 +42,7 @@
          
         </div> -->
         <div class="btn_Download">
-<button onclick="location.href='https://pvonq.zappar.io/4594993413743440733/9.0.0/'" >前往觀賞AR</button>
+<button onclick="location.href='https://pvonq.zappar.io/4594993413743440733/10.0.3/'" >前往觀賞AR</button>
        
         </div>
 
