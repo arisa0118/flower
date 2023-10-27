a0e6fd03c348e7d91c293fb1926456eb8bebf0f1
arisa_naiyun <arisa920118@gamil.com>
2023-10-27 23:52:30 +0800

    version8.8

--- uploadPicture.php
+++ uploadPicture.php
@@ -42,7 +42,7 @@
                     <label for="image">拍一張這裡的照片：</label><br><br>
                     <div class="fileUpload">
                     <input type="file" name="imgfile" data-target="preview_img" accept="image/gif,image/jpeg,image/png"
-                        required="required" class="upload">
+                        required="required" class="upload" onchange="upload(this)">
                         <span>Upload</span>
                         </div><br><br>
                     <label for="image" >預覽照片：</label><br><br>
@@ -76,6 +76,64 @@
                     }
                 }
         //圖片預覽script end
+        function upload(obj) {
+     var f = obj.files[0];
+     var image = new Image();
+     fr.onload = function () {
+        //裁剪
+        image.src = this.result;
+
+        var cvs = document.createElement('canvas');
+        var ctx = cvs.getContext('2d');
+        var scale = 1;
+        image.onload = function() {
+            var max_width = 750, max_height = 480;
+            //如果   [宽/高]   >  [最大宽/高]
+            if (image.width / image.height >= max_width / max_height) {
+                //如果 [宽]>[最大宽] 
+                if (image.width > max_width) {
+                    cvs.width = max_width;
+                    cvs.height = (image.height * max_width) / image.width;
+                } else {
+                    cvs.width = image.width;
+                    cvs.height = image.height;
+                }
+            } else {
+                //如果 [高]>[最大高] 
+                if (image.height > max_height) {
+                    cvs.height = max_height;
+                    cvs.width = (image.width * max_height) / image.height;
+                } else {
+                    cvs.width = image.width;
+                    cvs.height = image.height;
+                }
+            }
+
+            // if(image.width > 750 || image.height > 480){  
+            // //1000只是示例，可以根据具体的要求去设定    
+            //     if(image.width > image.height){    
+            //         scale = 750 / image.width;  
+            //     }else{    
+            //         scale = 485 / image.height;    
+            //     }    
+            // }  
+            // cvs.width = image.width*scale;    
+            // cvs.height = image.height*scale;     
+            //计算等比缩小后图片宽高  
+           
+            var mpImg = new MegaPixImage(image);
+            mpImg.render(cvs, {
+              maxWidth: 750,
+              maxHeight: 480,
+              quality: 0.8,
+              orientation: Orientation
+            });
+            //得到裁剪后base64位图片
+            var newImageData = cvs.toDataURL("image/jpeg", 0.8);
+            //ajax传到后台处理
+    }
+    
+ } 
 
             </script>
 
