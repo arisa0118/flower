370669ec71362fc75952140ed601365ad2f14dd9
arisa_naiyun <arisa920118@gamil.com>
2023-10-28 02:10:29 +0800

    version9.5

--- functions.php
+++ functions.php
@@ -70,7 +70,7 @@ imagejpeg($new_image,$filePlace, 'resized/'.$fileName);
     }else{
         $newImageName=uniqid();
         $newImageName .= '.'. $imageExtension;
-        move_uploaded_file($tmpname,'Pimg/'.$newImageName);
+        move_uploaded_file($filePlace,'Pimg/'.$newImageName);
         // Insert image file name into database
             //新添加一筆資料(準備)
             $sql = "INSERT INTO person_data (
